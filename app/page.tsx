4270f68d1866d2b6aa46663ddcc2afd11914f683
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2026-03-30 11:05:23 +0530

    theme sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl text-teal-600 font-bold underline">Hello world!</h1>
+  );
 }
